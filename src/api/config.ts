ef96acd862c5e8cf29b7b3345f92f61d415ac838
wangkai53 <wangkai53@sfmail.sf-express.com>
2021-06-04 09:14:39 +0800

    fix(api/config.ts): axios 不同环境下baseurl配置出错

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Toast } from 'vant';
 
 // axios.defaults.baseURL = 'https://netease-cloud-music-api-three-sage.vercel.app';
-axios.defaults.baseURL = import.meta.env.MODE === 'development' ? '/api': '';
+axios.defaults.baseURL = import.meta.env.MODE === 'development' ? '/api': 'https://netease-cloud-music-api-three-sage.vercel.app';
 axios.defaults.timeout = 6000;
 axios.defaults.withCredentials = true;
 // 添加请求拦截器
